a054e756a14b183011a875d2a53a566e08707618
huynhdai1997 <73520742+huynhdai1997@users.noreply.github.com>
2020-12-31 15:47:44 +0700

    -  Load image from URL in file JSON: 100%

---
+++ LoadDataAnimal/LoadDataAnimal/Data/DataFood.swift
@@ -0,0 +1,17 @@
+//
+//  DataFood.swift
+//  LoadDataAnimal
+//
+//  Created by Dai Huynh on 12/30/20.
+//
+
+import Foundation
+
+struct DataFood: Codable {
+    
+    let name:    String
+    let calo:    String
+    let protein: Float
+    let image:   String
+    
+}
--- LoadDataAnimal/LoadDataAnimal/UI/ViewController.swift
+++ LoadDataAnimal/LoadDataAnimal/UI/ViewController.swift
@@ -8,62 +8,57 @@
 import UIKit
 
 extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
-    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+    func downloadImage(from url: URL) {
+        getData(from: url) {
+            data, response, error in
+            guard let data = data, error == nil else {return}
+            
+            DispatchQueue.main.async() {
+                self.image = UIImage(data: data)
+            }
+        }
     }
 }
 
 class ViewController: UIViewController  {
     
     @IBOutlet weak var loadDataCollectionView: UICollectionView!
-    let animalImage = "Big Foot"
-    let loadData = Bundle.main.decode(type: [DataAnimal].self, from: "newAnimal.json")
-    let colorArray: [UIColor] = [.systemRed, .systemYellow, .systemOrange, .systemGreen, .systemBlue, .blue, .systemPurple, .brown]
+    
+    let loadDataAnimal = Bundle.main.decode(type: [DataAnimal].self, from: "newAnimal.json")
+    let loadDataFood = Bundle.main.decode(type: [DataFood].self, from: "newFood.json")
+    let colorArray: [UIColor] = [.systemRed, .systemYellow, .systemOrange, .systemGreen,                                 .systemBlue, .blue, .systemPurple, .brown]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadDataCollectionView.dataSource = self
-        loadDataCollectionView.delegate = self
+        loadDataCollectionView.delegate   = self
     }
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return loadData.count
+        return loadDataAnimal.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LoadDataCell", for: indexPath) as! LoadDataCollectionViewCell
         let currentColorIndex = indexPath.row % colorArray.count
+        let urlAnimal = URL(string: loadDataAnimal[indexPath.row].image)
         
         cell.animalView.backgroundColor = colorArray[currentColorIndex]
-//        cell.animalImageView.image = UIImage(named: animalImage)
-        cell.animalImageView.downloaded(from: loadData[indexPath.row].image)
+        cell.animalImageView.downloadImage(from: urlAnimal!)
+        cell.lblName.text = loadDataAnimal[indexPath.row].name
+        cell.lblWeigh.text = loadDataAnimal[indexPath.row].weigh
+        cell.lblSpeed.text = loadDataAnimal[indexPath.row].speed
+        cell.lblOld.text = loadDataAnimal[indexPath.row].old
+        cell.lblFood.text = loadDataAnimal[indexPath.row].food
 
-        cell.lblName.text = loadData[indexPath.row].name
-        cell.lblWeigh.text = loadData[indexPath.row].weigh
-        cell.lblSpeed.text = loadData[indexPath.row].speed
-        cell.lblOld.text = loadData[indexPath.row].old
-        cell.lblFood.text = loadData[indexPath.row].food
-        
         return cell
-
     }
 }
 
